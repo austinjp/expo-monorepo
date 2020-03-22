79be9bd8e1fd11e2cfb31aa45c420a1f1c839e0a
austinjp <austin.plunkett@gmail.com>
2020-03-22 19:17:51 +0000

    Shorthand

--- babel.config.js
+++ babel.config.js
@@ -7,10 +7,10 @@ module.exports = {
       "plugins": [
         "transform-remove-console",
         [ '@babel/plugin-transform-react-jsx', { pragma: 'h' } ],
-        '@babel/plugin-syntax-dynamic-import',
+        'syntax-dynamic-import',
         // 'babel-plugin-transform-es2015-spread', // FIXME Still not working :(
-        '@babel/plugin-transform-runtime', // FIXME Note this is in .babelrc!
-        '@babel/plugin-transform-async-to-generator',
+        'transform-runtime', // FIXME Note this is in .babelrc!
+        'transform-async-to-generator',
       ]
     }
   }
